cfaad5ccc584a0ff1e0e26227c2673ddc605a1d3
brandonbaris <brandonbaris@gmail.com>
2015-02-27 11:30:39 -1000

    adds eraser and clear all button

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -5,10 +5,10 @@ function PixelPainter( width, height ){
   this.width = width;
   this.height = height;
   this.palette = [];
-  // this.render( width, height);
 }
 
 PixelPainter.prototype.render = function() {
+
   var mainGrid = $("<div>", { "class" : "big_grid"});
   for ( var i = 0; i < this.height; i++ ){
     var gridCol = $("<div>", { "class" : "grid_column" });
@@ -19,34 +19,29 @@ PixelPainter.prototype.render = function() {
     mainGrid.append ( gridCol );
   }
   $( "#container" ).append( mainGrid );
-
 };
 
-
 // color generator
-// function Colors() {
-// };
-
 PixelPainter.prototype.generate = function(){
   for ( var i = 0; i < 360; i +=5 ){
     this.palette.push(i);
   }
-  // console.log('palette',this.palette);
   return this.palette;
 };
 
 PixelPainter.prototype.renderColor = function(){
-  // var colorbox = [];
+
   var colorContainer = $("<div>", { "class" : "color_container" });
   for ( var i = 0; i < this.palette.length; i++ ){
-        var colorGrid = $("<div>", { "class" : "colorbox", "id" : "color_select" + [i] });
-        colorGrid.css( "background-color", "hsl(" + this.palette[i] + ",100%, 50%)");
-        // $("body").css( "background", "hsl(" + this.palette[i] + ",100%,50%);");
-        colorContainer.append( colorGrid );
-        // var parapara = $( "<p>", { html : "asdaf"});
-        $( "#container" ).append( colorContainer );
-      }
-      return true;
+    var colorGrid = $("<div>", { "class" : "colorbox", "id" : "color_select" + [i] });
+    colorGrid.css( "background-color", "hsl(" + this.palette[i] + ",100%, 50%)");
+    colorContainer.append( colorGrid );
+    $( "#container" ).append( colorContainer );
+  }
+  var erase = $( "<button>", { "id" : "eraser", html : "ERASER" });
+  var clearbutton = $( "<button>", { "id" : "clear_all", html : "CLEAR GRID" });
+  $( ".color_container" ).append( erase, clearbutton );
+  return true;
 };
 
 // }); //closure
--- js/app.js
+++ js/app.js
@@ -10,4 +10,40 @@ $( ".colorbox" ).click( function(){
   currentColor = $( this ).css( "background-color" );
   console.log(currentColor);
 });
+
+$( "#eraser" ).click( function(){
+  currentColor = "rgb(255,255,255)";
+  console.log(currentColor);
+});
+
+$( "#clear_all" ).click( function(){
+  $( ".gridbox" ).css( "background-color", "rgb(255,255,255)");
+});
+// jquery code plugin for disabling text selection
+// http://techlister.com/jquery/disable-and-enable-text-selection-using-jquery/854/
+$.fn.disableSelection = function() {
+        return this
+        .attr('unselectable', 'on')
+        .css('user-select', 'none')
+        .css('-moz-user-select', 'none')
+        .css('-khtml-user-select', 'none')
+        .css('-webkit-user-select', 'none')
+        .on('selectstart', false)
+        .on('contextmenu', false)
+        .on('keydown', false)
+        .on('mousedown', false);
+    };
+
+$( ".gridbox" ).disableSelection();
+
+$( ".gridbox" ).mousedown( function(){
+  $( ".gridbox" ).mouseover( function () {
+    if( currentColor!== undefined){
+      $( this ).css( "background-color", currentColor );
+    };
+    // $( ".gridbox" ).mouseup( function(){
+    //   return false;
+    // })
+  });
+});
 }); //closure
