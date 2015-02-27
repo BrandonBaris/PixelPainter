d83d1a21ded418d845c7a8691d1e61fcccf8da8c
brandonbaris <brandonbaris@gmail.com>
2015-02-26 20:07:03 -1000

    adds render

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -53,11 +53,26 @@ Colors.prototype.generate = function(){
     this.palette.push(i);
   }
   console.log('palette',this.palette);
+  return this.palette;
 };
 
 Colors.prototype.renderColor = function(){
+  // var colorbox = [];
+  for ( var i = 0; i < this.palette.length; i++ ){
 
+        var colorGrid = $("<div>", { "class" : "colorbox" });
+        colorGrid.css( "background-color", "hsl(" + this.palette[i] + ",100%, 50%)");
+        // $("body").css( "background", "hsl(" + this.palette[i] + ",100%,50%);");
+
+        console.log(colorGrid);
+        // var parapara = $( "<p>", { html : "asdaf"});
+        $( "#controls" ).append( colorGrid );
+      }
 };
 
 var swatch = new Colors();
 swatch.generate();
+// console.log(swatch.generate());
+swatch.renderColor();
+
+// "hsl(" + this.palette[i] + ",100%, 50%)"
