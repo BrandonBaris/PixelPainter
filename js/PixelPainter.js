5e05be726a3aeece1404c3c37fd40852d50e6b53
brandonbaris <brandonbaris@gmail.com>
2015-02-27 10:12:01 -1000

    merges Colors and PixelPainter classes

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,19 +1,19 @@
-$(function(){
+// $(function(){
 
 //pixelpainter grid
 function PixelPainter( width, height ){
   this.width = width;
   this.height = height;
+  this.palette = [];
   // this.render( width, height);
 }
 
 PixelPainter.prototype.render = function() {
   var mainGrid = $("<div>", { "class" : "big_grid"});
-  var gridCol = $("<div>", { "class" : "col" });
   for ( var i = 0; i < this.height; i++ ){
-
+    var gridCol = $("<div>", { "class" : "grid_column" });
     for ( var j = 0; j < this.width; j++ ){
-      var gridRow = $("<div>", { "class" : "gridbox"});
+      var gridRow = $("<div>", { "class" : "gridbox" });
       gridCol.append ( gridRow );
     }
     mainGrid.append ( gridCol );
@@ -24,25 +24,22 @@ PixelPainter.prototype.render = function() {
 
 
 // color generator
-function Colors() {
+// function Colors() {
+// };
 
-  this.palette = [];
-
-};
-
-Colors.prototype.generate = function(){
-  for ( var i = 0; i <= 360; i +=5 ){
+PixelPainter.prototype.generate = function(){
+  for ( var i = 0; i < 360; i +=5 ){
     this.palette.push(i);
   }
   // console.log('palette',this.palette);
   return this.palette;
 };
 
-Colors.prototype.renderColor = function(){
+PixelPainter.prototype.renderColor = function(){
   // var colorbox = [];
   var colorContainer = $("<div>", { "class" : "color_container" });
   for ( var i = 0; i < this.palette.length; i++ ){
-        var colorGrid = $("<div>", { "class" : "colorbox", html : "" });
+        var colorGrid = $("<div>", { "class" : "colorbox", "id" : "color_select" + [i] });
         colorGrid.css( "background-color", "hsl(" + this.palette[i] + ",100%, 50%)");
         // $("body").css( "background", "hsl(" + this.palette[i] + ",100%,50%);");
         colorContainer.append( colorGrid );
@@ -51,10 +48,5 @@ Colors.prototype.renderColor = function(){
       }
       return true;
 };
-var swatch = new Colors();
-swatch.generate();
-console.log('palette',swatch.palette);
-swatch.renderColor();
-var pixels = new PixelPainter( 30,30 );
-pixels.render();
-}); //closure
+
+// }); //closure
--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 $(function(){
+var currentColor; //color to draw with
+var pixels = new PixelPainter( 30,30 );
 
+pixels.generate();
+console.log('palette',pixels.palette);
+pixels.renderColor();
+pixels.render();
+$( ".colorbox" ).click( function(){
+  currentColor = $( this ).css( "background-color" );
+  console.log(currentColor);
+});
 }); //closure
